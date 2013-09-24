cf7a11a241e0d5506cf7348bd6986d8d57749506
Andrew Dumont <andrewjdumont@gmail.com>
2013-09-24 16:40:59 +0000

    Stride hootsuite integration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HootsuiteStrideApp::Application.config.secret_key_base = '6e7705ef294332b44cb2f527e3b8eac9532b3fa5f55fd228e6564b67f9d9bb63447efa73da7e7832dc3c48c1dc9c48df9a5234c40466f0d8e6085c8c57064417'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HootsuiteStrideApp::Application.config.session_store :cookie_store, key: '_hootsuite_stride_app_session'
